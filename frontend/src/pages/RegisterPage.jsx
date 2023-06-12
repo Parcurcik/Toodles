a639b44701813112d062160ba4c0106ef27e29b5
Dzharvizzz-coder <Dzharvizzz@yandex.ru>
2023-06-12 19:09:24 +0500

    fix validation on login menu

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import logo from '../images/reg_logo.png'
 import lines from '../images/lines.png'
 import RegisterMenu from "../components/RegisterMenu";
@@ -10,130 +10,131 @@ import Button from "../components/Button";
 import Register_words from '../images/Register_words.png'
 import axios from "axios";
 import * as yup from "yup"
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 
 const validationSchema = yup.object().shape({
-    email: yup.string().required("Введите адрес электронной почты"),
-    password: yup.string().required("Введите пароль"),
+  email: yup.string().required("Введите адрес электронной почты"),
+  password: yup.string().required("Введите пароль"),
 });
 
 const Register = () => {
-    const navigate = useNavigate();
-    const [menuActive, setMenuActive] = useState(false)
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState({});
+  const navigate = useNavigate();
+  const [menuActive, setMenuActive] = useState(false)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState({});
 
-    const setEmptyData = () => {
-        setEmail("");
-        setPassword("");
+  const setEmptyData = () => {
+    setEmail("");
+    setPassword("");
+  };
+
+  const isVisible = !menuActive;
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
     };
+  }, []);
 
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter' && event.target === document.body) {
+      event.preventDefault();
+      buttonRef.current.click();
+    }
+  };
 
-    const isVisible = !menuActive;
-      const buttonRef = useRef(null); // Добавление референса
+  const handleClick = () => {
+    setMenuActive(!menuActive);
+  }
 
-      useEffect(() => {
-        document.addEventListener('keydown', handleKeyDown);
-        return () => {
-          document.removeEventListener('keydown', handleKeyDown);
-        };
-      }, []);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await validationSchema.validate({
+        email,
+        password,
+      }, { abortEarly: false });
 
-      const handleKeyDown = (event) => {
-        if (event.key === 'Enter' && event.target === document.body) {
-          event.preventDefault();
-          buttonRef.current.click();
-        }
+      const data = {
+        email,
+        password,
       };
 
-      const handleClick = () => {
-        setMenuActive(!menuActive);
+      const response = await axios.post("http://127.0.0.1:5000/api/login", data);
+      const { access_token } = response.data;
+      localStorage.setItem('access_token', access_token);
+      navigate('/');
+
+      setEmptyData();
+
+    } catch (error) {
+      if (error instanceof yup.ValidationError) {
+        const errorMessages = {};
+
+        error.inner.forEach((validationError) => {
+          errorMessages[validationError.path] = validationError.message;
+        });
+
+        setError(errorMessages);
+      } else {
+        setError({ general: 'Неверный логин/пароль' });
       }
+    }
+  };
 
+  return (
+    <div className="menu_main_reg">
+      <div
+        style={{
+          position: "absolute",
+          width: "100vw",
+          height: "100vh",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          zIndex: "1"
+        }} />
+      <img
+        src={lines}
+        style={{
+          position: "absolute",
+        }}
+        alt={""}
+        className={"lines"}
+      />
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        try {
-            await validationSchema.validate({
-                email,
-                password,
-            }, { abortEarly: false });
+      <img
+        src={logo}
+        style={{}}
+        className={"logo_reg"}
+        alt={""}
+      />
 
-            const data = {
-                email,
-                password,
-            };
+      <InputReg placeHolder={"Почта"} image={letter_Reg} value={email}
+        onChange={(e) => setEmail(e.target.value)} />
+      {error.email && <span className="error-message_2">{error.email}</span>}
 
-            const response = await axios.post("http://127.0.0.1:5000/api/login", data);
-            const {access_token} = response.data;
-            localStorage.setItem('access_token', access_token);
-            navigate('/');
+      <InputReg placeHolder={"Пароль"} marginTop={"2%"} image={lock} input_type={"password"} value={password}
+        onChange={(e) => setPassword(e.target.value)} />
+      {error.password && <span className="error-message_2">{error.password}</span>}
+      {error.general && <span className="error-message_2">{error.general}</span>}
 
-            setEmptyData();
+      <Button Text={"Вход"} marginTop={"5%"} onClick={handleSubmit} buttonRef={buttonRef} />
 
-        } catch (error) {
-            if (error instanceof yup.ValidationError) {
-              const errorMessages = {};
+      {isVisible && (
+        <img
+          src={Register_words}
+          alt={"Зарегистрироваться"}
+          className={"reg_words"}
+          onClick={handleClick}
+        />
+      )}
 
-              error.inner.forEach((validationError) => {
-                errorMessages[validationError.path] = validationError.message;
-              });
-
-              setErrors(errorMessages);
-            }
-        }
-    };
-
-    return (
-        <div className="menu_main_reg">
-            <div
-                style={{
-                    position: "absolute",
-                    width: "100vw",
-                    height: "100vh",
-                    backgroundColor: "rgba(0, 0, 0, 0.5)",
-                    zIndex: "1"
-                }}/>
-            <img
-                src={lines}
-                style={{
-                    position: "absolute",
-                }}
-                alt={""}
-                className={"lines"}
-            />
-
-            <img
-                src={logo}
-                style={{}}
-                className={"logo_reg"}
-                alt={""}
-            />
-
-            <InputReg placeHolder={"Почта"} image={letter_Reg} value={email}
-                       onChange={(e) => setEmail(e.target.value)}/>
-            {errors.email && <span className="error-message_2">{errors.email}</span>}
-
-            <InputReg placeHolder={"Пароль"} marginTop={"2%"} image={lock} input_type={"password"} value={password}
-                       onChange={(e) => setPassword(e.target.value)}/>
-            {errors.password && <span className="error-message_2">{errors.password}</span>}
-
-            <Button Text={"Вход"} marginTop={"5%"} onClick={handleSubmit} buttonRef={buttonRef}/>
-
-            {isVisible && (
-                <img
-                    src={Register_words}
-                    alt={"Зарегистрироваться"}
-                    className={"reg_words"}
-                    onClick={handleClick}
-                />
-            )}
-
-            <RegisterMenu active={menuActive} setActive={setMenuActive}/>
-        </div>
-    )
+      <RegisterMenu active={menuActive} setActive={setMenuActive} />
+    </div>
+  )
 }
 
-export default Register
+export default Register;
